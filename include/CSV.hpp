54c789d3d39752d55c7b88804f827598dc8f2af8
MilkFather <31627231+MilkFather@users.noreply.github.com>
2018-11-01 13:02:44 +0800

    CSV dependency written

--- include/CSV.hpp
+++ include/CSV.hpp
@@ -1,12 +1,26 @@
 #ifndef CSV_HPP
 #define CSV_HPP
 
+#include <vector>
+#include <string>
+
 // 负责处理CSV文件格式的CSV类
 // 故名思意, Storage可以调用这里的方法进行CSV的读写与解析
 // 这样Storage就不用担心文件格式问题了, 将二者分离也是好事
 
-class CSV {
+using std::vector;
+using std::string;
 
+typedef vector<string> CSVRow;
+
+class CSV {
+public:
+    static vector<CSVRow> openFile(string filename);
+    static void writeFile(vector<CSVRow> table, string filename);
+    
+private:
+    static CSVRow parseLine(string l);
+    static string makeLine(CSVRow r);
 };
 
 #endif
--- src/CSV.cpp
+++ src/CSV.cpp
@@ -1,3 +1,67 @@
 #include "CSV.hpp"
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::endl;
 
 // CSV类的实现
+
+CSVRow CSV::parseLine(string l) {
+    CSVRow v;
+    string tmp;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ',') {
+            v.push_back(tmp);
+            tmp = "";
+        } else {
+            tmp += s[i];
+        }
+    }
+    if (tmp != "") {
+        v.push_back(tmp);
+    }
+    
+    for (int i = 0; i < v.size(); i++) {
+        v[i] = v[i].substr(1, v[i].length() - 2);
+    }
+    return v;
+}
+
+string CSV::makeLine(CSVRow r) {
+    if (r.size() <= 0) {
+        return "";
+    } else {
+        string s = "\"" + r[0] + "\"";
+        for (int i = 1; i < r.size(); i++) {
+            s = s + "," + "\"" + r[i] + "\"";
+        }
+        return s;
+    }
+}
+
+vector<CSVRow> CSV::openFile(string filename) {
+    vector<CSVRow> table;
+    ifstream f(filename);
+    if (!f.is_open()) {
+        return table;
+    } else {
+        string l;
+        while (getline(f, l)) {
+            CSVRow r = parseLine(l);
+            table.push_back(r);
+        }
+        f.close();
+        return r;
+    }
+}
+
+void CSV::writeFile(vector<CSVRow> table, string filename) {
+    ofstream f(filename);
+    if (!f.is_open()) {
+        for (int i = 0; i < table.size(); i++) {
+            f << makeLine(table[i]) << endl;
+        }
+        f.close();
+    }
+}
