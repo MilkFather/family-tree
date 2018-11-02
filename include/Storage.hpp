eaeccfb3fe292fc9cfa6e0b3d9191fca9ba4fbe3
MilkFather <31627231+MilkFather@users.noreply.github.com>
2018-11-02 22:31:12 +0800

    Update include/Storage.hpp

--- include/Storage.hpp
+++ include/Storage.hpp
@@ -5,6 +5,8 @@
 #include "Person.hpp"
 #include "Relation.hpp"
 
+using std::list;
+
 // 负责数据存储的Storage类
 // 看得眼熟?
 // 毕竟, 这就是仿照Agenda的模式写的项目嘛
@@ -23,7 +25,9 @@ public:
     
 private:
     static shared_ptr<Storage> m_instance;
-    list<>
+    list<Person> m_persons;
+    list<Relation> m_metarelations;
+    list<Relation> m_deducedrelations;
     bool m_dirty;
 };
 
